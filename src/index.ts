5f3465eaf5edc9c1f1500acffb98013df60264e3
justablob <me@blob.dev>
2021-01-04 20:33:50 +0100

    rename source files

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@
 
 import * as constants from "./constants";
 
-import Client from "./client";
-import Server from "./server";
+import Client from "./Client";
+import Server from "./Server";
 
-import * as Helper from "./helper";
+import * as Helper from "./Helper";
 
 export default {
   version: constants.VERSION,
